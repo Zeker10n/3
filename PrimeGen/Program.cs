a8274d566fa447e115e151d17aecc18a59ac45dc
Zeker10n <tristan.edward.hoenninger@gmail.com>
2021-10-15 23:29:22 -0400

    Program runs much faster now.

--- PrimeGen/Program.cs
+++ PrimeGen/Program.cs
@@ -21,9 +21,25 @@ using System.Threading.Tasks;
 
 namespace PrimeGen
 {
-    
+    /// <summary>
+    /// Runs the main for the program. Checks for valid arguments from the command line
+    /// and if they are invalid print help message. If they are vaild it generates
+    /// and prints a specified amount of prime numbers, the bit length of the numbers,
+    /// and the time it took to generate the numbers in a parallel manner.
+    /// </summary>
     class Program
     {
+        private static String helpMsg = ("dotnet run <bits> <count=1>\n" +
+                                  "- bits - the number of bits of the prime number, this must be a\n".PadLeft(68) +
+                                  "multiple of 8, and at least 32 bits.\n".PadLeft(43) +
+                                  "- count - the number of prime numbers to generate, defaults to 1".PadLeft(68));
+        /// <summary>
+        /// Checks for valid arguments from the command line and if they are invalid
+        /// print help message. If they are vaild it generates and prints a specified
+        /// amount of prime numbers, the bit length of the numbers, and the time it
+        /// took to generate the numbers in a parallel manner.
+        /// </summary>
+        /// <param name="args">Arguments from the command line</param>
         static void Main(string[] args)
         {
             if (args.Length >= 1 && args.Length < 3)
@@ -43,28 +59,22 @@ namespace PrimeGen
                 }
                 else
                 {
-                    HelpMsg();
+                    Console.WriteLine(helpMsg);
                 }
             }
             else
             {
-                HelpMsg();
+                Console.WriteLine(helpMsg);
             }
-        }
-        
-        /// <summary>
-        /// Prints a help message when given invalid arguments.
-        /// </summary>
-        static void HelpMsg()
-        {
-            Console.WriteLine("dotnet run <bits> <count=1>");
-            Console.WriteLine("- bits - the number of bits of the prime number, this must be a".PadLeft(67));
-            Console.WriteLine("multiple of 8, and at least 32 bits.".PadLeft(42));
-            Console.WriteLine("- count - the number of prime numbers to generate, defaults to 1".PadLeft(68));
         }
         
     }
 
+    /// <summary>
+    /// Class creates an instance of PrimeNum and generates and prints a specified
+    /// amount of prime numbers, the bit length of the numbers, and the time it
+    /// took to generate the numbers in a parallel manner.
+    /// </summary>
     class PrimeNum
     {
         public static object myLock = new Object();
@@ -79,31 +89,37 @@ namespace PrimeGen
         }
         
         /// <summary>
-        /// Generates a random BigInteger and returns it if its even and not divisible by 3, 5, or 7
-        /// and returns a -1 if it doesn't meet that criteria. 
+        /// Generates a random BigInteger and returns it if its even and not divisible by a
+        /// prime number within 100 and returns a -1 if it doesn't meet that criteria. 
         /// </summary>
         /// <param name="bitLen">The bit length of the BigInteger to generate</param>
-        /// <returns>Returns the generated number if its even and not divisible by 3, 5, or 7.
-        /// It returns -1 if these criteria aren't meet.</returns>
+        /// <returns>Returns the generated number if its not even and not divisible by a
+        ///  prime number with 100. It returns -1 if these criteria aren't meet.</returns>
         public BigInteger GenerateNum(int bitLen)
-        { 
+        {
+            int[] checkers = new int[] 
+                {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
             var rngCsp = new RNGCryptoServiceProvider();
             int numOfBytes = bitLen / 8;
             byte[] bytes = new byte[numOfBytes];
             rngCsp.GetBytes(bytes);
             var num = new BigInteger(bytes);
             num = BigInteger.Abs(num);
-            if (!num.IsEven && num % 5 != 0 && num % 3 != 0 && num % 7 != 0 )
+            foreach (var check in checkers)
             {
-                return num;
+                if (num % check == 0)
+                {
+                    return -1;
+                }
             }
 
-            return -1;
+            return num;
         }
         
         /// <summary>
         /// Generates and prints a specified number of prime numbers as it finds them and does so in a parallel method.
         /// Cancels the threads once the specified number of prime numbers have been printed.
+        /// The ar multiple checks to see if teh thread should be killed to ensure high speeds.
         /// </summary>
         /// <param name="bitLen">The bit length of the BigInteger object(s) that will be generated</param>
         /// <param name="numOfPrime">The number of prime numbers that need to be generated</param>
@@ -117,23 +133,30 @@ namespace PrimeGen
             {
                 Parallel.For(0, Int32.MaxValue, po, (i, state) =>
                     {
-                        if (po.CancellationToken.IsCancellationRequested && _numOfExec > numOfPrime)
+                        if (po.CancellationToken.IsCancellationRequested)
                         {
                             po.CancellationToken.ThrowIfCancellationRequested();
-                            state.Break();
                         }
                         else
                         {
                             var num = GenerateNum(bitLen);
+                            if (po.CancellationToken.IsCancellationRequested)
+                            {
+                                po.CancellationToken.ThrowIfCancellationRequested();
+                            }
                             if (num != -1)
                             {
+                                if (po.CancellationToken.IsCancellationRequested)
+                                {
+                                    po.CancellationToken.ThrowIfCancellationRequested();
+                                }
                                 if (num.IsProbablyPrime())
                                 {
+                                    
                                     lock (myLock)
                                     {
-                                        if (_numOfExec > numOfPrime)
+                                        if (po.CancellationToken.IsCancellationRequested)
                                         {
-                                            cts.Cancel();
                                             po.CancellationToken.ThrowIfCancellationRequested();
                                         }
                                         else if (_numOfExec < numOfPrime)
@@ -146,10 +169,16 @@ namespace PrimeGen
                                         {
                                             Console.WriteLine("{0}: {1}", _numOfExec, num);
                                             _numOfExec++;
+                                            cts.Cancel();
+                                            po.CancellationToken.ThrowIfCancellationRequested();
                                         }
 
                                     }
                                 }
+                            }
+                            if (po.CancellationToken.IsCancellationRequested)
+                            {
+                                po.CancellationToken.ThrowIfCancellationRequested();
                             }
                         }
                     });
@@ -179,7 +208,10 @@ namespace PrimeGen
         
     }
 
-    
+    /// <summary>
+    /// Holds extension methods for other classes to use. IsProbablyPrime checks if
+    /// the given value is prime using k number with the range of [2, value - 2] against value.
+    /// </summary>
     public static class MyExtensions
     {
         /// <summary>
@@ -200,7 +232,7 @@ namespace PrimeGen
                 r++;
             }
 
-            for (int i = 0; i < k; i++)
+            for (var i = 0; i < k; i++)
             {
                 BigInteger a = default;
                 do
@@ -216,7 +248,7 @@ namespace PrimeGen
                     continue;
                 }
 
-                for (int j = 0; j < r - 1; j++)
+                for (var j = 0; j < r - 1; j++)
                 {
                     x = BigInteger.ModPow(x, 2, n);
                     if (x == 1)
